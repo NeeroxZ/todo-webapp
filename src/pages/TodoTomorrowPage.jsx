e1f5c70360add445470f4ca79916ae52460dcbd8
Marvin Samouelian <samouelian@raceresult.com>
2023-01-27 20:53:45 +0100

    [New]: Added new pages with todo filters

---
+++ src/pages/TodoTomorrowPage.jsx
@@ -0,0 +1,21 @@
+import {TodoPage} from "./TodoPage";
+
+export const TodoTomorrowPage = () => {
+    let time = new Date();
+    time.setUTCDate(time.getDate() + 1);
+    let tomorrow = (time.getUTCFullYear() + "-" + ('0' + (time.getUTCMonth() + 1)).slice(-2) + '-' + ('0' + time.getUTCDate()).slice(-2));
+
+    const getTimeFrom = () => {
+        return `${tomorrow} 00:00:00.000`;
+    }
+
+    const getTimeUntil = () => {
+        return `${tomorrow} 23:59:59.999`;
+    };
+
+    return (
+        <>
+            <TodoPage dateFrom={getTimeFrom()} dateUntil={getTimeUntil()} />
+        </>
+    );
+};
