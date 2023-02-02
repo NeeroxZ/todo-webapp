2ef53fe9f05096b85412176f0fc0f05368dbb130
NeeroxZ <neeroxz@hotmail.de>
2023-02-02 11:33:30 +0100

    Navbar test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,26 +5,22 @@ import {Outlet} from "react-router-dom";
 import 'font-awesome/css/font-awesome.min.css';
 export const NavBar = () => {
 
-    const [open, setOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setOpen(!open);
-    };
 
 
     return (
         <>
             <header>
                 <div className="logo">LOGO</div>
-                <nav className={`${open?"active":""}`}>
+                <nav>
                     <ul>
-                        <li><a href="#">Home</a></li>
+                        <li><a href="#" className="active">Home</a></li>
                         <li><a href="#">About</a></li>
                         <li><a href="#">Services</a></li>
                         <li><a href="#">Contact</a></li>
                     </ul>
                 </nav>
-                <div onClick={toggleMenu} className="menu-toggle"><i className="fa fa-bars"></i></div>
+                <div className="menu-toggle"><i className="fa fa-bars"></i></div>
             </header>
             <Outlet />
        </>
