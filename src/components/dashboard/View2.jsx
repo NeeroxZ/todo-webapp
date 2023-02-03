decb52eb5ce6204dc1eabe01c6113bc27158cd4c
NeeroxZ <neeroxz@hotmail.de>
2023-02-03 11:57:09 +0100

    Dashboard design change

---
+++ src/components/dashboard/View2.jsx
@@ -0,0 +1,11 @@
+
+export const View2 = () => {
+    return(
+        <div className="item-c dash-box">
+            <div>
+                <h5 className="header">Due ToDO</h5>
+                <div className="cash-font-md gradient-font" data-target="1194.09">4 steht für Lifestyle</div>
+            </div>
+        </div>
+    )
+}
