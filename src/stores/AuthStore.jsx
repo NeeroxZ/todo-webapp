5d2b22a81554f06ee3fc9878cbb6de33ea3d6ff5
Marvin Samouelian <samouelian@raceresult.com>
2023-02-03 11:08:51 +0100

    [Fix]: Auth now stored as cookies and loaded

--- src/stores/AuthStore.jsx
+++ src/stores/AuthStore.jsx
@@ -1,6 +1,7 @@
-import {useState, createContext, useContext} from "react";
+import {useState, createContext, useContext, useEffect} from "react";
 import pb from "../utils/pocketbase";
 import {useNavigate} from "react-router-dom";
+import Cookies from "universal-cookie/es6";
 
 const AuthContext = createContext(null);
 
@@ -8,8 +9,34 @@ export const AuthProvider = ({children}) => {
     const [waiting, setWaiting] = useState(false);
     const [loginError, setLoginError] = useState(null);
     const [loginSuccess, setLoginSuccess] = useState(false);
+    const [loginValid, setLoginValid] = useState(false);
 
     const navigator = useNavigate();
+
+    const cookies = new Cookies();
+    const COOKIE_KEY = "auth";
+
+
+    useEffect(() => {
+        const loadCookie = async () => {
+            try {
+                let foundCookie = cookies.get(COOKIE_KEY)
+                pb.authStore.loadFromCookie(foundCookie, COOKIE_KEY);
+                try {
+                    pb.authStore.isValid && await pb.collection('users').authRefresh();
+                    setLoginValid(true);
+                } catch (e) {
+                    setLoginValid(false);
+                    pb.authStore.clear();
+                }
+
+            }
+            catch (e){
+                console.log("could not find/parse auth cookie");
+            }
+        };
+        loadCookie().then(() => console.log("cookie checked"));
+    }, []);
 
 
     // login
@@ -22,6 +49,8 @@ export const AuthProvider = ({children}) => {
                 username,
                 password
             )
+            let expCookie = pb.authStore.exportToCookie({sameSite: 'none', secure: false}, COOKIE_KEY);
+            cookies.set(COOKIE_KEY, expCookie, {sameSite:'none', secure:false})
         } catch(error) {
             setWaiting(false);
             setLoginError(true);
@@ -34,7 +63,7 @@ export const AuthProvider = ({children}) => {
             setLoginError(true);
             return
         }
-
+        setLoginValid(true);
         setWaiting(false);
         setLoginSuccess(true);
         navigator("/home");
@@ -42,16 +71,19 @@ export const AuthProvider = ({children}) => {
 
     // logout
     const logout = async () => {
+        cookies.remove("auth");
+        setLoginValid(false);
         pb.authStore.clear()
         navigator("/login")
     };
+
 
     const getUserId = () => {
         return pb.authStore.model.id;
     };
 
     return(
-        <AuthContext.Provider value={{login, logout, loginError, waiting, loginSuccess, getUserId}}>
+        <AuthContext.Provider value={{login, logout, loginError, waiting, loginSuccess, getUserId, loginValid}}>
             {children}
         </AuthContext.Provider>
     )
