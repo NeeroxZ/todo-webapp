def50e2916a7ea664f1f22efabc311a78e4873c6
NeeroxZ <neeroxz@hotmail.de>
2023-02-16 12:45:45 +0100

    UserPage

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -13,20 +13,37 @@ export const UserPage = () => {
            <div className="containerUser">
                <div className="rowLogin">
                    <form className="box">
-                       <h1 className="form-name">Sign In</h1>
-                       <label>
-                           <input className="placeholders" type="text" placeholder="Username"
+                       <p className="form-name">Change User Information</p>
+                       <div className="userRow">
+                               <h1>UserName</h1>
+                               <input className="userInput" type="text" placeholder="Username"
+                                      onChange={e => setUsername(e.target.value)}
+                               />
+                       </div>
+                       <div className="userRow">
+                               <h1>E-Mail</h1>
+                               <input className="userInput" type="email" placeholder="E-Mail"
+                                      onChange={e => setUsername(e.target.value)}
+                               />
+                       </div>
+                       <div className="userRow">
+                           <h1>Password</h1>
+                           <input className="userInput" type="password" placeholder="Password"
                                   onChange={e => setUsername(e.target.value)}
-                                  />
-                       </label>
-                       <label>
-                           <input className="placeholders" type="password" placeholder="Password"
-                                  onChange={e => setPassword(e.target.value)}
-                                  />
-                       </label>
-                       <input className="btn-submit" type="submit" name="" value="Sign In"
+                           />
+                       </div>
+                       <div className="userRow">
+                           <h1>Bockmark instead of DUE</h1>
+                           <input type="checkbox"/>
+                       </div>
+                       <div className="userRow">
+                           <h1>Disable Done Todo`s</h1>
+                           <input type="checkbox" className="checkBoxInput"/>
+                           <span className="checkmark"></span>
+                       </div>
+                       <input className="btn-submit" type="submit" name="" value="Save"></input>
+                       <input className="btn-delete" type="submit" name="" value="Delete"></input>
 
-                       />
                    </form>
                </div>
            </div>
