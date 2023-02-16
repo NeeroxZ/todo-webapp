11776c5228744d7aafb382965434cd0b9cf0d268
NeeroxZ <neeroxz@hotmail.de>
2023-02-16 11:29:07 +0100

    Changes by marvinsamouelian

--- src/App.js
+++ src/App.js
@@ -22,64 +22,75 @@ import {TodoTomorrowPage} from "./pages/TodoTomorrowPage";
 import {Dashboard} from "./pages/Dashboard";
 import {NavBar} from "./components/Navbar";
 import {UserPage} from "./pages/UserPage";
+import {createTheme, ThemeProvider} from "@mui/material";
+import {About} from "./pages/About";
 
 
-
+const theme = createTheme({
+    palette:{
+        mode: 'dark',
+        primary: {
+            main: "#04AA6D",
+        },
+    }
+});
 
 function App() {
     return (
         <>
-
             <GlobalStore>
-                <Router>
-                    <AuthProvider>
-                        <NavBar/>
-                        <Routes>
-                            <Route index element={
-                                <ProtectedRoute>
-                                    <Dashboard />
-                                </ProtectedRoute>
-                            }/>
-                            <Route path="login" element={<Login/>}/>
-                            <Route path="reset" element={<ResetPasswordPage/>}/>
-                            <Route path="register" element={<Register/>}/>
-                            <Route path="confirm" element={<ConfirmMailPage/>}/>
-                            <Route path="dashboard" element={<Dashboard/>}/>
-                            <Route path="user" element={<UserPage/>}/>
-                            <Route path="home" element={
-                                <ProtectedRoute>
-                                    <Dashboard/>
-                                </ProtectedRoute>}/>
-                            <Route path="todo">
-                                <Route path="all" element={
+                <ThemeProvider theme={theme}>
+                    <Router>
+                        <AuthProvider>
+                            <NavBar/>
+                            <Routes>
+                                <Route index element={
                                     <ProtectedRoute>
-                                        <TodoPage scrollable={true}/>
+                                        <Dashboard/>
                                     </ProtectedRoute>
                                 }/>
-                                <Route path="today" element={
+                                <Route path="login" element={<Login/>}/>
+                                <Route path="reset" element={<ResetPasswordPage/>}/>
+                                <Route path="register" element={<Register/>}/>
+                                <Route path="confirm" element={<ConfirmMailPage/>}/>
+                                <Route path="dashboard" element={<Dashboard/>}/>
+                                <Route path="user" element={<UserPage/>}/>
+                                <Route path="about" element={<About/>}/>
+                                <Route path="home" element={
                                     <ProtectedRoute>
-                                        <TodoTodayPage/>
+                                        <Dashboard/>
+                                    </ProtectedRoute>}/>
+                                <Route path="todo">
+                                    <Route path="all" element={
+                                        <ProtectedRoute>
+                                            <TodoPage scrollable={true}/>
+                                        </ProtectedRoute>
+                                    }/>
+                                    <Route path="today" element={
+                                        <ProtectedRoute>
+                                            <TodoTodayPage/>
+                                        </ProtectedRoute>
+                                    }/>
+                                    <Route path="tomorrow" element={
+                                        <ProtectedRoute>
+                                            <TodoTomorrowPage/>
+                                        </ProtectedRoute>
+                                    }/>
+                                </Route>
+                                <Route path="topic/:title" element={
+                                    <ProtectedRoute>
+                                        <TodoTopicPage/>
                                     </ProtectedRoute>
                                 }/>
-                                <Route path="tomorrow" element={
-                                    <ProtectedRoute>
-                                        <TodoTomorrowPage/>
-                                    </ProtectedRoute>
-                                }/>
-                            </Route>
-                            <Route path="topic/:title" element={
-                                <ProtectedRoute>
-                                    <TodoTopicPage/>
-                                </ProtectedRoute>
-                            }/>
 
-                            <Route path="*" element={<NoMatch/>}/>
-                        </Routes>
-                    </AuthProvider>
-                </Router>
+                                <Route path="*" element={<NoMatch/>}/>
+                            </Routes>
+                        </AuthProvider>
+                    </Router>
+                </ThemeProvider>
             </GlobalStore>
         </>
-    )
+    );
 }
 
 
