240c38fb2e31ff2262a906f8f65f1bbf42f7cc75
Marvin Samouelian <samouelian@raceresult.com>
2023-02-16 18:59:04 +0100

    [Moved]: Component for missing todos and topics

---
+++ src/pages/NoContent.jsx
@@ -0,0 +1,45 @@
+import "../styles/todo.css"
+import {AddTodo} from "../components/AddTodo";
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import PropTypes from 'prop-types';
+import {useEffect, useState} from "react";
+export const NoContent = (props) => {
+
+    const [isTopic, setIsTopic] = useState(false);
+
+    useEffect(() => {
+        if (props.variant === "topic") {
+            setIsTopic(true);
+        }
+    }, []);
+
+
+    return(
+        <>
+            <div className="noTodoContainer">
+                <div className="noTodo">
+                    {isTopic
+                        ? (<p>Ohh, it`s seems like that topic does not exists</p>)
+                        : (<p>Ohh, it`s seems you have no ToDo`s</p>)
+                    }
+                </div>
+            </div>
+
+            {/*<div className="ocean">*/}
+            {/*    <div className="wave"></div>*/}
+            {/*    <div className="wave"></div>*/}
+            {/*</div>*/}
+            <div className="arrow">
+                <ArrowDownwardIcon
+                    fontSize="large"
+                    color="primary"
+                />
+            </div>
+            <AddTodo/>
+        </>
+    )
+}
+
+NoContent.propTypes = {
+    variant: PropTypes.string,
+}
