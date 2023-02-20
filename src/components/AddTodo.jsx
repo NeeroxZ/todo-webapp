0e0c9ad105df3bb76cada96c569459af76cb97b4
Marvin Samouelian <samouelian@raceresult.com>
2023-02-20 12:15:10 +0100

    [Placeholder]: Nothing new

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -4,43 +4,19 @@ import {Fab} from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import {AddModal} from "./AddModal";
 import {useState} from "react";
-import PropTypes from 'prop-types';
 
 
-
-export const AddTodo = (props) => {
+export const AddTodo = () => {
     const [isOpen, setIsOpen] = useState(false);
-    console.log(props.loading)
     return (
-        <>
-            <AddModal
-                show={isOpen}
-                setShow={setIsOpen}
-                reloadOnAdd={props.reloadOnAdd}
-                reloadFunction={props.reloadFunction}
-                loading={props.loading}
-                selectedTopic={props.selectedTopic}
-            />
-            <div className={"fixedRightContainer"}>
-
-                <Fab size="large" color="primary" aria-label="add" className="fab" onClick={() => {
-                    setIsOpen(true);
-                }}>
+            <>
+            <AddModal show={isOpen} setShow={setIsOpen}/>
+                <div className="modalPopup">
+                    <Fab size="medium" color="primary" aria-label="add" className="fab" onClick={() => {
+                        setIsOpen(true);}}>
                     <AddIcon/>
-                </Fab>
-            </div>
-        </>
-    );
-}
-
-AddTodo.defaultProps = {
-    reloadOnAdd: false,
-};
-
-AddTodo.propType = {
-    show: PropTypes.bool.isRequired,
-    setShow: PropTypes.func.isRequired,
-    reloadOnAdd: PropTypes.bool,
-    reloadFunction: PropTypes.func,
-    loading: PropTypes.bool,
+                    </Fab>
+                </div>
+            </>
+        );
 }
