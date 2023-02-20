8ce4fb2f513b8c2d71a960f7e9153235a0b97396
Marvin Samouelian <samouelian@raceresult.com>
2023-02-20 12:08:12 +0100

    [Changed]: theme name

--- src/theme/theme.jsx
+++ src/theme/theme.jsx
@@ -1,10 +1,17 @@
 import {createTheme} from "@mui/material";
 
 export const Theme = createTheme({
-    palette:{
+    palette: {
         mode: 'dark',
         primary: {
             main: "#04AA6D",
+        },
+
+        textWhite: {
+            light: '#fdfdfdce',
+            main: '#fdfdfdce',
+            dark: '#fdfdfdce',
+            contrastText: 'rgba(0, 0, 0, 0.87)',
         },
     }
 });
