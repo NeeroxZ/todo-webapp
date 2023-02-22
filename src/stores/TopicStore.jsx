342bf1409f006d7c5a72c547a8aed5f25df6116e
Marvin Samouelian <samouelian@raceresult.com>
2023-02-22 23:57:35 +0100

    [Changed]: only fetch if user is authenticated

--- src/stores/TopicStore.jsx
+++ src/stores/TopicStore.jsx
@@ -12,37 +12,46 @@ export const TopicProvider = ({children}) => {
     const [topics, setTopics] = useState([]);
 
     const loadTopics = async () => {
-        console.log("get topics");
-        setError(false);
-        setWaiting(true);
-        try{
-            let foundTopics = await pb.collection('topics').getFullList(200, {
-                filter: `user_id="${getUserId()}"`,
-                sort: '-created'
-            })
-            let list = [];
-            foundTopics.forEach(elem => {
-                list.push({
-                    id: elem.id,
-                    title: elem.title,
-                    titleLow: elem.title.toLowerCase(),
-                    titleParam: elem.title.toLowerCase().replaceAll(" ", "_"),
-                    titleMod: modifyTopic(elem.title),
+        console.log("loginValid: ", auth.loginValid)
+        if (auth.loginValid) {
+            console.log("get topics");
+            setError(false);
+            setWaiting(true);
+            try{
+                let foundTopics = await pb.collection('topics').getFullList(200, {
+                    filter: `user_id="${getUserId()}"`,
+                    sort: '-created'
                 })
-            });
-            setTopics(list);
-            setWaiting(false);
-            console.log("got topics");
-        } catch (e) {
-            console.log("error gettings topics: ", e);
-            setError(e);
-            setWaiting(false);
+                let list = [];
+                foundTopics.forEach(elem => {
+                    list.push({
+                        id: elem.id,
+                        title: elem.title,
+                        titleLow: elem.title.toLowerCase(),
+                        titleParam: elem.title.toLowerCase().replaceAll(" ", "_"),
+                        titleMod: modifyTopic(elem.title),
+                    })
+                });
+                setTopics(list);
+                setWaiting(false);
+                console.log("got topics");
+            } catch (e) {
+                console.log("error gettings topics: ", e);
+                setError(e);
+                setWaiting(false);
+            }
         }
     };
 
     useEffect(() => {
+        // if (auth.loginValid) {
             loadTopics();
+        // }
     }, [auth.loginValid]);
+
+    // if (auth.loginValid) {
+    //     loadTopics();
+    // }
 
 
     return (
