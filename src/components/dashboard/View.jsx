14a26b48194fe338e59dcd8796ccd76acadb54c1
NeeroxZ <neeroxz@hotmail.de>
2023-03-01 19:59:39 +0100

    Add Todo length

--- src/components/dashboard/View.jsx
+++ src/components/dashboard/View.jsx
@@ -1,10 +1,22 @@
+import {useEffect} from "react";
+import {useTodoState} from "../../hooks/useTodoState";
+import {useAuth} from "../../stores/AuthStore";
 
-export const View = () => {
+
+
+export const View = (props) => {
+    const auth = useAuth();
+    const tds = useTodoState(props);
+    useEffect( () => {
+        tds.loadTodos(props);
+    }, [props.topic, auth.loginValid]);
+
+
     return(
         <div className="item-c dash-box">
             <div>
                 <a className="">All ToDo`s:</a>
-                <div className="cash-font-md gradient-font" data-target="1194.09">69</div>
+                <div className="cash-font-md gradient-font allNmb" data-target="1194.09">{tds.todos.length}</div>
             </div>
         </div>
     )
