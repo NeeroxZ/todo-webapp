e22ff567160bada5a5d234b17fc2d5f2cdf2e678
NeeroxZ <neeroxz@hotmail.de>
2023-03-01 20:00:35 +0100

    Get User name (BUG)

--- src/components/dashboard/User.jsx
+++ src/components/dashboard/User.jsx
@@ -1,16 +1,20 @@
 import { Responsive, WidthProvider } from "react-grid-layout";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import {useAuth} from "../../stores/AuthStore";
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
 export const User = () => {
+    const auth = useAuth();
+    const name = auth.getUserId();
+
     return (
         <div className="item-a dash-box">
             <a href="#">
                 <AccountCircleIcon/>
             </a>
             <div className="username">
-                NeeroxZ
+                {name}
             </div>
         </div>
     );
