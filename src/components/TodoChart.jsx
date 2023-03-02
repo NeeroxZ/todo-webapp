be1fa917abb1614d5363e031bf6c3b5a8e89403e
NeeroxZ <neeroxz@hotmail.de>
2023-03-02 11:12:48 +0100

    Charts update

--- src/components/TodoChart.jsx
+++ src/components/TodoChart.jsx
@@ -1,25 +1,59 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Chart} from "react-google-charts";
-
-const tasks = [
-    ["Status", "Count"],
-    ["Finished", 4],
-    ["Unfinished", 7],
-    ["Expired", 2]
-];
+import {useTodoState} from "../hooks/useTodoState";
+import {useAuth} from "../stores/AuthStore";
+import Todo from "./Todo";
 
 const opts = {
     title: "ToDo´s status",
+    defaultColors: [ '#00ccff', '#008800', '#ddaacc', /*Array of colors*/ ]
 };
 
-export function TodoChart() {
+export function TodoChart(props) {
+    const auth = useAuth();
+    const tds = useTodoState(props);
+    const getTimeUntil = () => {
+        return new Date().getUTCFullYear();
+    };
+    let dueValue = 0;
+    let day =   new Date().getTime() / 1000
+    let done = 0;
+    useEffect( () => {
+        tds.loadTodos(props);
+    }, [props.topic, auth.loginValid]);
+
+    {tds.todos && tds.todos.map((item) => {
+        let dmp = new Date(item.due_date).getTime() / 1000
+        if(day >= dmp){
+            dueValue ++;
+        }
+        if(item.done){
+            done++;
+        }
+    })}
+
+    const tasks = [
+        ["Status", "Count"],
+        ["ToDo`s", tds.todos.length - dueValue],
+        ["Unfinished", tds.todos.length - done],
+        ["Expired", dueValue]
+    ];
+    console.log(dueValue + " duuuuuues");
+
+    console.log(tds.todos);
     return (
-        <Chart
-            chartType="PieChart"
-            data={tasks}
-            options={opts}
-            width="100%"
-            height="100%"
-        />
+        <>
+            <div className="chats">
+                <Chart
+                    chartType="PieChart"
+                    data={tasks}
+                    options={opts}
+                    width="100%"
+                    height="100%"
+                    className={'charts'}
+
+                />
+            </div>
+        </>
     );
 }
